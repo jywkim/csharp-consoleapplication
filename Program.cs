f9e6ee555b4e9d27e53565669a2077505478e922
jywkim <jkim589@gmail.com>
2018-09-05 22:46:13 -0400

    add delays and format output

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Threading.Tasks;
 
 namespace TeleprompterConsole
 {
@@ -11,7 +12,15 @@ namespace TeleprompterConsole
             var lines = ReadFrom("sampleQuotes.txt");
             foreach (var line in lines)
             {
-              Console.WriteLine(line);
+              Console.Write(line);
+              if (!string.IsNullOrWhiteSpace(line))
+              {
+                  var pause = Task.Delay(200);
+                  // Synchronously waiting on a task is an
+                  // anti-pattern. This will get fixed in later
+                  // steps.
+                  pause.Wait();
+              }
             }
         }
 
@@ -22,7 +31,19 @@ namespace TeleprompterConsole
             {
                 while ((line = reader.ReadLine()) != null)
                 {
-                    yield return line;
+                    var words = line.Split(' ');
+                    var lineLength = 0;
+                    foreach (var word in words)
+                    {
+                      yield return word + " ";
+                      lineLength += word.Length + 1;
+                      if (lineLength > 70)
+                      {
+                          yield return Environment.NewLine;
+                          lineLength = 0;
+                      }
+                    }
+                    yield return Environment.NewLine;
                 }
             }
         }
